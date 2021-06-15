dad8c4f59fbb7700cb71d6c567d93db2cfc2f053
atarubift <atarubift@gmail.com>
2021-06-15 10:05:50 +0900

    Add Game Class

--- main.rb
+++ main.rb
@@ -1,9 +1,22 @@
 require 'dxopal'
 include DXOpal
-Window.load_resources do
-  Window.bgcolor = C_BLACK
 
-  Window.loop do
-    Window.draw_font(0, 0, "Hello!", Font.default, color: C_WHITE)
+X_WIDTH = Window.width
+Y_HEIGHT = Window.height 
+
+class Game
+  def initialize
   end
+
+  def run
+    Window.loop do
+      Window.draw_font(0, 0, "Hello!", Font.default, color: C_WHITE)
+    end
+  end
+end
+
+
+Window.load_resources do
+  game = Game.new
+  game.run
 end
